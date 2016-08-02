9b1c6612ca0045f52cdd44e54a1dbfefdec1be69
martincostello <martin@martincostello.com>
2016-08-02 21:20:06 +0100

    Improve Windows build scripting
    Improve the build scripting for Windows by switching to PowerShell,
    fetching the latest .NET CLI locally if not present, building to a
    dedicated output directory, using --no-build where prudent and stopping
    the build after the first error.

--- Build.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-dotnet restore src/Website/project.json tests/Website.Tests/project.json --verbosity minimal
-dotnet build src/Website
-dotnet test tests/Website.Tests
-dotnet publish src/Website
