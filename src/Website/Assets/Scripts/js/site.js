21845940ecc3abcb81f9bf4a5b59d9cc67823d30
martincostello <martin@martincostello.com>
2016-08-04 13:04:50 +0100

    Add function to send GA events
    Add a function to the site JavaScript to send Google Analytics events if
    it has been loaded.

--- src/Website/Assets/Scripts/js/site.js
+++ src/Website/Assets/Scripts/js/site.js
@@ -1,6 +1,11 @@
 ﻿martinCostello = {
     website: {
         branch: $("meta[name='x-site-branch']").attr("content"),
-        revision: $("meta[name='x-site-revision']").attr("content")
+        revision: $("meta[name='x-site-revision']").attr("content"),
+        track: function (category, action, label, value, fields) {
+            if ("ga" in window) {
+                ga("send", "event", category, action, label, value, fields);
+            }
+        }
     }
 };
