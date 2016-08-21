0923879317812912abb8287d87bde1d266087dc2
martincostello <martin@martincostello.com>
2016-08-21 16:07:35 +0100

    Return boolean from track function
    Return a boolean from the track() function so the caller knows if the
    event was actually tracked.

--- src/Website/Assets/Scripts/js/martinCostello/website/track.js
+++ src/Website/Assets/Scripts/js/martinCostello/website/track.js
@@ -8,9 +8,13 @@
  * @param {string} label - The event label.
  * @param {string} [value] - The optional event value.
  * @param {object} [fields] - The optional event data.
+ * @returns {Boolean} - Whether the analytics event was tracked.
  */
 martinCostello.website.track = function (category, action, label, value, fields) {
     if ("ga" in window) {
         ga("send", "event", category, action, label, value, fields);
+        return true;
+    } else {
+        return false;
     }
 };
