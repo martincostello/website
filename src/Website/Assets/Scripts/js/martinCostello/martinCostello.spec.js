6f8705fd292e21773d032ddfb75f10949ba8ad27
martincostello <martin@martincostello.com>
2016-08-21 11:06:47 +0100

    Add Jasmine test infrastructure
    Add infrastructure for running Jasmine tests in either PhantomJS or
    Chrome.

---
+++ src/Website/Assets/Scripts/js/martinCostello/martinCostello.spec.js
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+describe("Given the namespaces are defined", function () {
+
+    beforeAll(function () {
+    });
+
+    beforeEach(function () {
+    });
+
+    afterEach(function () {
+    });
+
+    afterAll(function () {
+    });
+
+    it("then martinCostello is not null", function () {
+        expect(martinCostello).not.toBeNull();
+    });
+
+    it("then martinCostello.website is not null", function () {
+        expect(martinCostello.website).not.toBeNull();
+    });
+
+    it("then martinCostello.website.tools is not null", function () {
+        expect(martinCostello.website.tools).not.toBeNull();
+    });
+});
