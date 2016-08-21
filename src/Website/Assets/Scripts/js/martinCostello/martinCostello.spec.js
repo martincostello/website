23ea139fc7963eae29f3937870a402036db9dc37
martincostello <martin@martincostello.com>
2016-08-21 16:12:37 +0100

    Add simple JavaScript tests
    Add some simple Jasmine tests for the JavaScript.

--- src/Website/Assets/Scripts/js/martinCostello/martinCostello.spec.js
+++ src/Website/Assets/Scripts/js/martinCostello/martinCostello.spec.js
@@ -3,18 +3,6 @@
 
 describe("Given the namespaces are defined", function () {
 
-    beforeAll(function () {
-    });
-
-    beforeEach(function () {
-    });
-
-    afterEach(function () {
-    });
-
-    afterAll(function () {
-    });
-
     it("then martinCostello is not null", function () {
         expect(martinCostello).not.toBeNull();
     });
@@ -25,5 +13,84 @@ describe("Given the namespaces are defined", function () {
 
     it("then martinCostello.website.tools is not null", function () {
         expect(martinCostello.website.tools).not.toBeNull();
+    });
+});
+
+describe("Google Analytics", function () {
+
+    describe("Given ga is not defined", function () {
+
+        it("then an event is not published", function () {
+
+            var category = "category";
+            var action = "action";
+            var label = "label";
+            var value = 1;
+            var fields = {
+                foo: "bar"
+            };
+
+            var result = martinCostello.website.track(category, action, label, value, fields);
+
+            expect(result).toBe(false);
+        });
+    });
+
+    describe("Given ga is defined", function () {
+
+        beforeEach(function () {
+            ga = jasmine.createSpy("ga");
+        });
+
+        afterEach(function () {
+            ga = null;
+        });
+
+        it("then an event is published", function () {
+
+            var category = "category";
+            var action = "action";
+            var label = "label";
+            var value = 1;
+            var fields = {
+                foo: "bar"
+            };
+
+            var result = martinCostello.website.track(category, action, label, value, fields);
+
+            expect(result).toBe(true);
+            expect(ga).toHaveBeenCalledWith("send", "event", category, action, label, value, fields);
+        });
+    });
+});
+
+describe("Debugging", function () {
+
+    xdescribe("Given meta tags containing the site version", function () {
+
+        beforeAll(function () {
+            $("head").append("<meta name='x-site-branch'] content='master' />");
+            $("head").append("<meta name='x-site-revision'] content='012345ab' />");
+        });
+
+        it("then the branch is correct", function () {
+            expect(martinCostello.website.debug.branch()).toBe("master");
+        });
+
+        it("then the revision is correct", function () {
+            expect(martinCostello.website.debug.revision()).toBe("012345ab");
+        });
+    });
+
+    describe("When logging", function () {
+
+        beforeEach(function () {
+            spyOn(console, "log");
+        });
+
+        it("then a message is logged", function () {
+            martinCostello.website.debug.log("2 + 2 = ", 2 + 2);
+            expect(console.log).toHaveBeenCalledWith("2 + 2 = ", 4);
+        });
     });
 });
