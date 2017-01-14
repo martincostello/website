2769635407303d11101e159e4ec3aa2229b7ec94
martincostello <martin@martincostello.com>
2017-01-14 17:41:13 +0000

    Only attempt to cache GET requests
    Only attempt to cache requests for HTTP GETs in the Service Worker.

--- src/Website/wwwroot/service-worker.js
+++ src/Website/wwwroot/service-worker.js
@@ -28,6 +28,9 @@ self.addEventListener("activate", function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
+    if (event.request.method !== "GET") {
+        return;
+    }
     event.respondWith(
         caches.match(event.request)
             .then(function (response) {
