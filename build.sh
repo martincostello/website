365935365fda25ead1799b46e8df18eb3ba83f02
martincostello <martin@martincostello.com>
2017-01-31 11:23:20 +0000

    Update Unix build script Update project paths to .csproj. Fail on first error.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-dotnet restore --verbosity minimal
-dotnet build src/Website
-dotnet test tests/Website.Tests
-dotnet publish src/Website
+dotnet restore --verbosity minimal || exit 1
+dotnet build src/Website/Website.csproj || exit 1
+dotnet test tests/Website.Tests/Website.Tests.csproj || exit 1
+dotnet publish src/Website/Website.csproj || exit 1
