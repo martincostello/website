13fbb2a218340ace2f220e090718de2f803bc537
martincostello <martin@martincostello.com>
2017-01-31 13:01:06 +0000

    Explicit restore packages for each project Restore NuGet packages per project instead of at the root.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-dotnet restore --verbosity minimal || exit 1
-dotnet build src/Website/Website.csproj || exit 1
+dotnet restore src/Website/Website.csproj --verbosity minimal || exit 1
+dotnet restore tests/Website.Tests/Website.Tests.csproj --verbosity minimal || exit 1
+dotnet build  src/Website/Website.csproj || exit 1
 dotnet test tests/Website.Tests/Website.Tests.csproj || exit 1
 dotnet publish src/Website/Website.csproj || exit 1
