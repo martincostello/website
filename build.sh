561532e8e2b5fe97a54a103505445ed975fd96c5
martincostello <martin@martincostello.com>
2017-02-11 12:53:00 +0000

    Build Release configuration

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 export artifacts=$(dirname "$(readlink -f "$0")")/artifacts
+export configuration=Release
 
 dotnet restore --verbosity minimal || exit 1
-dotnet build --output $artifacts || exit 1
-dotnet test tests/Website.Tests/Website.Tests.csproj --output $artifacts || exit 1
-dotnet publish src/Website/Website.csproj --output $artifacts || exit 1
+dotnet build --output $artifacts --configuration $configuration || exit 1
+dotnet test tests/Website.Tests/Website.Tests.csproj --output $artifacts --configuration $configuration || exit 1
+dotnet publish src/Website/Website.csproj --output $artifacts --configuration $configuration || exit 1
