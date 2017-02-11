3d22e1e4a8086695266942004197b8a3dc8cf59a
martincostello <martin@martincostello.com>
2017-02-11 12:23:25 +0000

    Use shared output directory for build Use shared output directory for build in Travis CI.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 dotnet restore --verbosity minimal || exit 1
-dotnet build  src/Website/Website.csproj || exit 1
-dotnet test tests/Website.Tests/Website.Tests.csproj || exit 1
-dotnet publish src/Website/Website.csproj || exit 1
+dotnet build --output ./artifacts || exit 1
+dotnet test tests/Website.Tests/Website.Tests.csproj --output ./artifacts || exit 1
+dotnet publish src/Website/Website.csproj --output ./artifacts || exit 1
