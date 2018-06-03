91cb9498559a8678dcdda927b4b551e5f81800d7
martincostello <martin@martincostello.com>
2018-06-03 16:31:01 +0100

    Remove HPKP
    Remove all references to the now-deprecated HTTP Public Key Pins response headers.

--- src/Website/Options/ReportOptions.cs
+++ src/Website/Options/ReportOptions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
+// Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 namespace MartinCostello.Website.Options
@@ -34,15 +34,5 @@ namespace MartinCostello.Website.Options
         /// Gets or sets the URI to use for <c>Expect-Staple</c>.
         /// </summary>
         public Uri ExpectStaple { get; set; }
-
-        /// <summary>
-        /// Gets or sets the URI to use for <c>Public-Key-Pins</c>.
-        /// </summary>
-        public Uri PublicKeyPins { get; set; }
-
-        /// <summary>
-        /// Gets or sets the URI to use for <c>Public-Key-Pins-Report-Only</c>.
-        /// </summary>
-        public Uri PublicKeyPinsReportOnly { get; set; }
     }
 }
