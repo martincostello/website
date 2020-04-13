39e1629fc8910d8360a8bdfc68f85be81687f2d9
martincostello <martin@martincostello.com>
2020-04-13 11:48:03 +0100

    Further GitHub Actions updates
    Further updates to support GitHub Actions.

--- build.ps1
+++ build.ps1
@@ -84,7 +84,7 @@ function DotNetTest {
     $reportGeneratorVersion = (Select-Xml -Path $propsFile -XPath "//PackageReference[@Include='ReportGenerator']/@Version").Node.'#text'
     $reportGeneratorPath = Join-Path $nugetPath "reportgenerator\$reportGeneratorVersion\tools\netcoreapp3.0\ReportGenerator.dll"
 
-    $coverageOutput = Join-Path $OutputPath "coverage.cobertura.xml"
+    $coverageOutput = Join-Path $OutputPath "coverage.*.cobertura.xml"
     $reportOutput = Join-Path $OutputPath "coverage"
 
     & $dotnet test $Project --output $OutputPath
