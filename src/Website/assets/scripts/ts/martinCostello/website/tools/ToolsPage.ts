647b43272115ad55601d2a5a97b63b6d2b1f1c16
martincostello <martin@martincostello.com>
2021-05-19 18:50:37 +0100

    Remove @types/clipboard
    It ships with its own type definitions now, but using that instead makes the compilation fail and I don't really know how to fix it so meh.

--- src/Website/assets/scripts/ts/martinCostello/website/tools/ToolsPage.ts
+++ src/Website/assets/scripts/ts/martinCostello/website/tools/ToolsPage.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+declare var ClipboardJS: any;
+
 namespace martinCostello.website.tools {
 
     /**
