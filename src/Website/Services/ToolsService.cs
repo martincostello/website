d07e9a74af39bc8aa9b6df4323ca5b4106fd1abd
martincostello <martin@martincostello.com>
2021-06-23 12:46:50 +0100

    Add global usings
    Add global usings with C# 10.

--- src/Website/Services/ToolsService.cs
+++ src/Website/Services/ToolsService.cs
@@ -1,9 +1,6 @@
 // Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.Security.Cryptography;
 using System.Text;
 using MartinCostello.Website.Models;
--- tests/Website.Tests/Integration/HttpServerFixture.cs
+++ tests/Website.Tests/Integration/HttpServerFixture.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System;
-using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
