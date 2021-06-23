f5834aa5d68e3061929c24586a2bbb9789eb716f
martincostello <martin@martincostello.com>
2021-06-23 12:46:50 +0100

    Add global usings
    Add global usings with C# 10.

--- tests/Website.Tests/Integration/UI/ToolPageTests.cs
+++ tests/Website.Tests/Integration/UI/ToolPageTests.cs
@@ -1,12 +1,7 @@
 // Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System;
-using System.Threading.Tasks;
 using MartinCostello.Website.Pages;
-using Shouldly;
-using Xunit;
-using Xunit.Abstractions;
 
 namespace MartinCostello.Website.Integration.UI
 {
--- tests/Website.Tests/Pages/PageBase.cs
+++ tests/Website.Tests/Pages/PageBase.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Martin Costello, 2016. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Threading.Tasks;
-
 namespace MartinCostello.Website.Pages
 {
     public abstract class PageBase
