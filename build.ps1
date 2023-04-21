7a96a1068e8e2a92016791c9116c3e9ee96212bb
martincostello <martin@martincostello.com>
2023-04-21 17:17:50 +0100

    Set UseArtifactsOutput=true
    Use the new `UseArtifactsOutput` property for the output paths and adjust the build scripts accordingly.

--- build.ps1
+++ build.ps1
@@ -1,7 +1,6 @@
 #! /usr/bin/env pwsh
 param(
     [Parameter(Mandatory = $false)][string] $Configuration = "Release",
-    [Parameter(Mandatory = $false)][string] $OutputPath = "",
     [Parameter(Mandatory = $false)][switch] $SkipTests,
     [Parameter(Mandatory = $false)][string] $Runtime = ""
 )
@@ -13,11 +12,6 @@ $solutionPath = $PSScriptRoot
 $sdkFile = Join-Path $solutionPath "global.json"
 
 $dotnetVersion = (Get-Content $sdkFile | Out-String | ConvertFrom-Json).sdk.version
-
-if ($OutputPath -eq "") {
-    $OutputPath = Join-Path $PSScriptRoot "artifacts"
-}
-
 $installDotNetSdk = $false;
 
 if (($null -eq (Get-Command "dotnet" -ErrorAction SilentlyContinue)) -and ($null -eq (Get-Command "dotnet.exe" -ErrorAction SilentlyContinue))) {
@@ -82,7 +76,7 @@ function DotNetTest {
         $additionalArgs += "GitHubActions;report-warnings=false"
     }
 
-    & $dotnet test $Project --output $OutputPath --configuration $Configuration $additionalArgs
+    & $dotnet test $Project --configuration $Configuration $additionalArgs
 
     if ($LASTEXITCODE -ne 0) {
         throw "dotnet test failed with exit code $LASTEXITCODE"
@@ -92,8 +86,6 @@ function DotNetTest {
 function DotNetPublish {
     param([string]$Project)
 
-    $publishPath = (Join-Path $OutputPath "publish")
-
     $additionalArgs = @()
 
     if (![string]::IsNullOrEmpty($Runtime)) {
@@ -102,7 +94,7 @@ function DotNetPublish {
         $additionalArgs += $Runtime
     }
 
-    & $dotnet publish $Project --output $publishPath --configuration $Configuration $additionalArgs
+    & $dotnet publish $Project $additionalArgs
 
     if ($LASTEXITCODE -ne 0) {
         throw "dotnet publish failed with exit code $LASTEXITCODE"
@@ -120,7 +112,7 @@ $publishProjects = @(
 
 Write-Host "Publishing solution..." -ForegroundColor Green
 ForEach ($project in $publishProjects) {
-    DotNetPublish $project $Configuration $PrereleaseSuffix
+    DotNetPublish $project
 }
 
 if ($SkipTests -eq $false) {
