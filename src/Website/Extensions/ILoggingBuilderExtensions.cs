c53b1c3085c4ecbd3be1445369b66b5eb965fc4e
martincostello <martin@martincostello.com>
2025-07-22 16:23:00 +0100

    Simplify OTLP configuration
    Configure OTLP for all signals centrally.

--- src/Website/Extensions/ILoggingBuilderExtensions.cs
+++ src/Website/Extensions/ILoggingBuilderExtensions.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using MartinCostello.Website;
-using OpenTelemetry.Logs;
 
 namespace Microsoft.Extensions.Logging;
 
@@ -26,11 +25,6 @@ public static class ILoggingBuilderExtensions
             options.IncludeScopes = true;
 
             options.SetResourceBuilder(ApplicationTelemetry.ResourceBuilder);
-
-            if (ApplicationTelemetry.IsOtlpCollectorConfigured())
-            {
-                options.AddOtlpExporter();
-            }
         });
     }
 }
